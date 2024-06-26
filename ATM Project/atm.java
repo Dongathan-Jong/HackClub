9ba1bf96a7b98e6f2cd3f67018d2685cf42b8a0e
Dongathan-Jong <duclingsyt@gmail.com>
2024-06-26 12:40:24 -0400

    Update atm.java

--- ATM Project/atm.java
+++ ATM Project/atm.java
@@ -1,1 +1,68 @@
 
+public class atm
+{
+   public static void main(String[] args)
+   {
+      
+      while (true)
+      {
+         
+         int userPin;
+         String userNumber;
+         int counter;
+         String[] values;
+         String passBy = ",";
+         String line;
+         String fileName;
+         Scanner sc = new Scanner(System.in);
+         String currentLine;      
+         userNumber=" ";
+         userPin=0;
+         
+         
+         try
+         {
+            
+            
+            System.out.print("Please enter an account number: ");
+            
+            userNumber = sc.nextLine(); 
+            
+        
+            fileName = Integer.toString(Integer.parseInt(userNumber)) + ".csv";
+         
+           
+            BufferedReader input = new BufferedReader(new FileReader(fileName));
+            
+            line = input.readLine();
+            
+            values = line.split(passBy, -1);
+            
+            System.out.print("Please enter the pin: ");
+            userPin = sc.nextInt();
+               
+            if (userPin == Integer.parseInt(values[1].replaceAll("\s+", "")))
+            {
+               input.close();
+               menuOfOptions(Integer.parseInt(userNumber), values);
+            }
+            else
+            {
+               System.out.println("Incorrect pin!");
+            }
+            
+         }
+         catch (FileNotFoundException e)
+         {
+            newProfile();
+         }
+         catch(IOException e)
+         {
+            System.out.print(e + "ERROR!! Something went wrong while opening a file!");
+         }
+         catch(NumberFormatException e)
+         {
+            newProfile();
+         }
+      }  
+   }
